f13b9e175abcc49e8a12de5683b23f33d8407402
vaibhav <vaibhavgoel841@gmail.com>
2023-10-14 12:49:41 +0530

    first commit

---
+++ config.py
@@ -0,0 +1,15 @@
+config = {
+    "google_api_key": "AIzaSyDqP8i4LLAJbx7Bb510R8u05yUCe-KLj_c",
+    "youtube_playlist_id": "PLgUwDviBIf0qUlt5H_kiKYaNSqJ81PMMY",
+    "kafka": {
+        "bootstrap.servers": "pkc-xrnwx.asia-south2.gcp.confluent.cloud:9092",
+        "security.protocol": "sasl_ssl",
+        "sasl.mechanism": "PLAIN",
+        "sasl.username": "VVZVRMD2MTDTAGGY",
+        "sasl.password": "KaTFSMLTXa9X+1M/8nWCWOuIpPC1ewWHNYVYffQK4JltJLWGfEJ1MS8vwefOTrrx",
+    },
+    "schema_registry": {
+        "url": "https://psrc-10wzj.ap-southeast-2.aws.confluent.cloud",
+        "basic.auth.user.info": "A5BEOLCXMLBBY7LZ:aemLNnRWuzTZlGMkTAfWPVRNCsI6S8a6yUn15U62AyJppNWf5Q89djoYMIICRjjr",
+    }
+}
